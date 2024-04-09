9d4193bca59ed57965a75c2e526939ec3587e44c
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-04-09 10:17:42 -0600

    Feat: add Form states and create form submission func

--- src/ResForm/ResForm.js
+++ src/ResForm/ResForm.js
@@ -1,6 +1,23 @@
 import './ResForm.css'
+import { useState } from 'react';
 
 function ResForm() {
+    const [name, setName] = useState('')
+    const [date, setDate] = useState('')
+    const [time, setTime] = useState('')
+    const [guests, setGuests] = useState('')
+
+    const createRes = (e) => {
+        e.preventdefault()
+        const newRes = {
+            name: name,
+            date: date,
+            time: time,
+            number: guests
+        }
+        addRes(newRes)
+    }
+
     return (
         <div>
             <form>
@@ -8,7 +25,7 @@ function ResForm() {
                 <input type='text' placeholder='Date(mm/dd)'></input>
                 <input type='text' placeholder='Time'></input>
                 <input type='text' placeholder='Number of guests'></input>
-                <button type='submit'>Make Reservation</button>
+                <button type='submit' onClick={(e) => createRes(e)}>Make Reservation</button>
             </form>
         </div>
     )
